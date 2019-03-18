62acba91e696228f6e51bdfbe70add6fbd83c8e4
dMaluenda <dmaluendan@gmail.com>
2019-03-18 18:27:03 +0100

    bugfixing

--- setup.py
+++ setup.py
@@ -145,7 +145,7 @@ setup(
     # MANIFEST.in as well.
     # include_package_data=True,
     package_data={  # Optional
-       'xmipp3': ['xmipp.png', 'protocols.conf'],
+      # 'xmipp3': ['xmipp_logo.png', 'protocols.conf'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
