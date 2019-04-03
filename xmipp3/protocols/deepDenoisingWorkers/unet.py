2195cbc6d07479ad067734bc07263449c925e627
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2019-04-03 15:03:29 +0200

    new options to deepDenoising

--- xmipp3/protocols/deepDenoisingWorkers/unet.py
+++ xmipp3/protocols/deepDenoisingWorkers/unet.py
@@ -1,78 +1,90 @@
 import sys, os
 import keras
 import math
-from .DeepLearningGeneric import DeepLearningModel
+from keras.models import load_model
+import tensorflow as tf
+
+from .DeepLearningGeneric import DeepLearningModel, save_imgs
 from .dataGenerator import getDataGenerator, extractNBatches, BATCH_SIZE
 
-NUM_BATCHES_PER_EPOCH= 50
+NUM_BATCHES_PER_EPOCH= 256
+
+CUSTOM_OBJECTS={}
 class UNET(DeepLearningModel):
   
-  def __init__(self, boxSize, saveModelDir, gpuList="0", batchSize=BATCH_SIZE, init_n_filters=64):
+  def __init__(self, boxSize, saveModelFname, gpuList="0", batchSize=BATCH_SIZE, modelDepth=4, generatorLoss="MSE",
+                     trainingDataMode="ParticlesAndSyntheticNoise"):
   
-    DeepLearningModel.__init__(self,boxSize, saveModelDir, gpuList, batchSize)
+    DeepLearningModel.__init__(self,boxSize, saveModelFname, gpuList, batchSize, generatorLoss= generatorLoss,
+                                trainingDataMode=trainingDataMode)
     self.epochSize= NUM_BATCHES_PER_EPOCH
-    self.init_n_filters= init_n_filters   
-           
+    
   def _setShape(self, boxSize):
-    self.shape= (boxSize,boxSize,1)
-    return self.shape
+    self.img_shape= (boxSize,boxSize,1)
+    return self.img_shape
     
   def train(self, learningRate, nEpochs, xmdParticles, xmdProjections):
-      
-    N_GPUs= len(self.gpuList.split(',')) 
 
-    if os.path.isfile(self.saveModelDir):
+    saveImagesPath= self.createSaveImgsPath(xmdParticles)
+    N_GPUs= len(self.gpuList.split(',')) 
+    if os.path.isfile(self.saveModelFname):
       print("loading previous model")
       if N_GPUs>1:
         with tf.device('/cpu:0'):
-          model_1gpu = load_model(self.saveModelDir, custom_objects= customLayers)
+          model_1gpu = load_model(self.saveModelFname, custom_objects= CUSTOM_OBJECTS)
         model= keras.utils.multi_gpu_model(model_1gpu, gpus= N_GPUs)
       else:
-        model_1gpu = load_model(self.saveModelDir, custom_objects= customLayers)
+        model_1gpu = load_model(self.saveModelFname, custom_objects= CUSTOM_OBJECTS)
         model= model_1gpu
     else:
+      paramsNewUnet={"img_shape":self.img_shape, "out_ch":1, "start_ch":32, "depth": self.modelDepth,
+                     "inc_rate":2., "activation":"relu", "dropout":0.5, "batchnorm":True, 
+                     "maxpool":True, "upconv":True, "residual":True, "lastActivation":"linear"}      
       if N_GPUs>1:
         with tf.device('/cpu:0'):
-          model_1gpu= build_UNet( img_shape=self.shape, start_ch=self.init_n_filters, batchnorm=False )
+          model_1gpu= build_UNet( **paramsNewUnet)
         model= keras.utils.multi_gpu_model(model_1gpu, gpus= N_GPUs)
       else:
-        model_1gpu= build_UNet( img_shape=self.shape, start_ch=self.init_n_filters, batchnorm=False )
+        model_1gpu= build_UNet( **paramsNewUnet)
         model= model_1gpu
     
-      optimizer= keras.optimizers.Adam(lr= learningRate, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=0)
-      model.compile(loss='mse', optimizer=optimizer)
-      
-      reduceLrCback= keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1, mode='auto', min_lr=1e-8)
-      earlyStopCBack= keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1, mode='auto')
-      saveModel= AltModelCheckpoint(self.saveModelDir, model, monitor='val_loss', verbose=1, save_best_only=True)
+    optimizer= keras.optimizers.Adam(lr= learningRate, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=0)
+    model.compile(loss=self.generatorLoss, metrics=["mse"], optimizer=optimizer)
 
-      trainIterator, stepsPerEpoch= getDataGenerator(xmdParticles, xmdProjections, isTrain=True, 
-                                                     valFraction=0.1, batchSize=self.batchSize)
-      print("train/val split"); sys.stdout.flush()
-      valIterator, stepsPerEpoch_val= getDataGenerator(xmdParticles, xmdProjections, augmentData=False, 
-                                                       isTrain=False, valFraction=0.1, batchSize=self.batchSize)
-      valData= extractNBatches(valIterator, min(10, stepsPerEpoch_val))
-      del valIterator
-#      valData=None
-      nEpochs_init= nEpochs
-      nEpochs= max(1, nEpochs_init*float(stepsPerEpoch)/self.epochSize)
-      print("nEpochs : %.1f --> Epochs: %d.\nTraining begins: Epoch 0/%d"%(nEpochs_init, nEpochs, nEpochs))
-      sys.stdout.flush()
-      
-      model.fit_generator(trainIterator, epochs= nEpochs, steps_per_epoch=self.epochSize, #steps_per_epoch=stepsPerEpoch,
-                        verbose=2, callbacks=[reduceLrCback, earlyStopCBack, saveModel],
-                        validation_data=valData, max_queue_size=12, workers=1, use_multiprocessing=False)
+    print("train/val split"); sys.stdout.flush()
+    valIterator, stepsPerEpoch_val= getDataGenerator(xmdParticles, xmdProjections, augmentData=False, 
+                                                     isTrain=False, valFraction=0.1, batchSize=self.batchSize)
+    valData= extractNBatches(valIterator, min(10, stepsPerEpoch_val)); del valIterator
+    
+    trainIterator, stepsPerEpoch= getDataGenerator(xmdParticles, xmdProjections, isTrain=True, augmentData=True,
+                                                   valFraction=0.1, batchSize=self.batchSize, doTanhNormalize=False,
+                                                   simulateEmptyParts=self.addSyntheticEmpty)            
+    cBacks=[]
+    cBacks+= [ keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, mode='auto', min_lr=1e-8) ]
+    cBacks+= [ keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto') ]
+    cBacks+= [ AltModelCheckpoint(self.saveModelFname, model_1gpu, monitor='val_loss', verbose=1, save_best_only=True) ]
+    cBacks+= [ WriteImageCBack(valData[0][:self.batchSize], valData[1][:self.batchSize], 
+                                saveImagesPath, batch_size=self.batchSize) ]
+    
+    nEpochs_init= nEpochs
+    nEpochs= max(1, nEpochs_init*float(stepsPerEpoch)/self.epochSize)
+    print("nEpochs : %.1f --> Epochs: %d.\nTraining begins: Epoch 0/%d"%(nEpochs_init, nEpochs, nEpochs))
+    sys.stdout.flush()
+    
+    model.fit_generator(trainIterator, epochs= nEpochs, steps_per_epoch=self.epochSize, #steps_per_epoch=stepsPerEpoch,
+                      verbose=2, callbacks=cBacks, validation_data=valData, max_queue_size=12, 
+                      workers=1, use_multiprocessing=False)
                         
 
 
 def build_UNet( img_shape, out_ch=1, start_ch=32, depth=3, inc_rate=2., activation='relu', 
-    dropout=0.5, batchnorm=False, maxpool=True, upconv=True, residual=False):
+    dropout=0.5, batchnorm=False, maxpool=True, upconv=True, residual=False, lastActivation="linear"):
     
-
     def conv_block( m, dim, acti, bn, res, do=0):
         n = keras.layers.Conv2D(dim, 3, activation=acti, padding='same')(m)
         n = keras.layers.BatchNormalization()(n) if bn else n
         n = keras.layers.Dropout(do)(n) if do else n
+#        n = keras.layers.SpatialDropout2D(do)(n) if do else n
         n = keras.layers.Conv2D(dim, 3, activation=acti, padding='same')(n)
         n = keras.layers.BatchNormalization()(n) if bn else n
         return keras.layers.Concatenate()([m, n]) if res else n
@@ -94,25 +106,18 @@ def build_UNet( img_shape, out_ch=1, start_ch=32, depth=3, inc_rate=2., activati
         return m
     
     i = keras.layers.Input(shape=img_shape)
-    PAD_SIZE= ( 2**(int(math.log(img_shape[1], 2))+1)-img_shape[1]) //2
-    x = keras.layers.ZeroPadding2D( (PAD_SIZE, PAD_SIZE) )( i ) #Padded to 2**N
+
+    all_pad_size= ( 2**(int(math.ceil(math.log(img_shape[1], 2) )))-img_shape[1])
+    pad_size_left=  all_pad_size//2
+    pad_size_right=  all_pad_size//2 + all_pad_size%2
+
+    x = keras.layers.ZeroPadding2D( [(pad_size_left, pad_size_right), (pad_size_left, pad_size_right) ])( i ) #Padded to 2**N
+
     o = level_block(x, start_ch, depth, inc_rate, activation, dropout, batchnorm, maxpool, upconv, residual)
-    o = keras.layers.Conv2D(out_ch, 1, activation='linear')(o)
-    o = keras.layers.Lambda(lambda m: m[:,PAD_SIZE:-PAD_SIZE,PAD_SIZE:-PAD_SIZE,:] )( o )
+    o = keras.layers.Conv2D(out_ch, 1, activation=lastActivation)(o)
+    if pad_size_right>0:
+      o = keras.layers.Lambda(lambda m: m[:,pad_size_left:-pad_size_right,pad_size_left:-pad_size_right,:] )( o )
     return  keras.models.Model(inputs=i, outputs=o)
-
-
-def segmentationLoss(trn_labels_batch, logits):
-  '''
-  '''
-  logits=tf.reshape(logits, [-1])
-  trn_labels=tf.reshape(trn_labels_batch, [-1])
-  inter=tf.reduce_sum(tf.multiply(logits,trn_labels))
-  union=tf.reduce_sum(tf.subtract(tf.add(logits,trn_labels),tf.multiply(logits,trn_labels)))
-  loss=tf.subtract(tf.constant(1.0, dtype=tf.float32),tf.div(inter,union))
-  return loss
-
-
 
 #####################################################################################################################
 # AltModelCheckpoint taken from https://github.com/TextpertAi/alt-model-checkpoint/blob/master/alt_model_checkpoint/__init__.py#L9     #
@@ -139,4 +144,21 @@ class AltModelCheckpoint(ModelCheckpoint):
         self.model = self.alternate_model
         super(type(self),self).on_epoch_end(epoch, logs)
         self.model = model_before
+        
+
+class WriteImageCBack(keras.callbacks.Callback):
+  """"
+  callback to observe the output of the network
+  """
+  def __init__(self, x, y, saveImagesPath, batch_size= 16):
+    self.x = x
+    self.y = y
+    self.batch_size= batch_size
+    self.saveImagesPath= saveImagesPath
+    self.currentEpoch=0
+  def on_epoch_end(self, epoch, logs={}):
+      self.currentEpoch+=1
+      pred= self.model.predict(self.x, batch_size= self.batch_size, verbose=0)
+      save_imgs([pred, self.x, self.y], ["generated", "particles", "projections"],
+               self.saveImagesPath, self.currentEpoch)
         
