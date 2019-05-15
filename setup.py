a75c030606adcf6aadd730e7bc4e2cd70feab4b3
dMaluenda <dmaluendan@gmail.com>
2019-05-15 11:13:28 +0200

    Updating pypi version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='19.04.01',  # Required
+    version='19.04.02',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
