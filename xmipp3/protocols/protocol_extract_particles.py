d6585ce217ff150a23667c8e8659d53445ab37f4
dMaluenda <dmaluendan@gmail.com>
2019-10-16 16:55:03 +0200

    Extracting even particles, always III

--- xmipp3/protocols/protocol_extract_particles.py
+++ xmipp3/protocols/protocol_extract_particles.py
@@ -453,7 +453,7 @@ class XmippProtExtractParticles(ProtExtractParticles, XmippProtocol):
         return f / self.downFactor.get() if self._doDownsample() else f
 
     def getEven(self, boxSize):
-        return Integer(int(boxSize.get()/2+0.75)*2)
+        return Integer(int(int(boxSize)/2+0.75)*2)
 
     def getBoxSize(self):
         # This function is needed by the wizard and for auto-boxSize selection
