9170fe00bbb6a4cdba2df309229b40ea6c25bf91
DavidHerreros <david.herreroscalero@gmail.com>
2019-11-22 17:02:44 +0100

    Solved issues when working with several tomograms and other improvements

--- xmipp3/protocols/__init__.py
+++ xmipp3/protocols/__init__.py
@@ -120,4 +120,5 @@ from .protocol_angular_alignment_sph import XmippProtAngularAlignmentSPH
 from .protocol_volume_deform_sph import XmippProtVolumeDeformSPH
 from .protocol_structure_map_sph import XmippProtStructureMapSPH
 from .protocol_align_volume_and_particles import XmippProtAlignVolumeParticles
+from .protocol_project_top import XmippProtProjectZ
 
