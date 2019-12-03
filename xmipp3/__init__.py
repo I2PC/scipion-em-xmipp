43b5550597af99f40af908650fb93ebf35544c08
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2019-12-03 13:07:11 +0100

    * Decode the outputs

--- xmipp3/__init__.py
+++ xmipp3/__init__.py
@@ -241,7 +241,7 @@ def installDeepLearningToolkit(plugin, env):
         except:
             nvccVersion = 'None'  # string to avoid 'NoneType is not iterable' error
 
-        nvccVersion = [nvcc.encode("utf8") for nvcc in nvccVersion]
+        nvccVersion = [nvccV.decode("utf8") for nvccV in nvccVersion]
 
         if "release 8.0" in nvccVersion:  # cuda 8
             tensorFlowTarget = "1.4.1"
