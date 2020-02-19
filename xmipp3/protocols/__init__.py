337118b5727f229c99ae1f31af4b55a4a624509d
dMaluenda <dmaluendan@gmail.com>
2020-02-19 19:30:51 +0100

    Removing 3Dbionotes

--- xmipp3/protocols/__init__.py
+++ xmipp3/protocols/__init__.py
@@ -28,7 +28,6 @@
 
 from .protocol_preprocess import *
 
-from .protocol_3dbionotes import XmippProt3DBionotes
 from .protocol_assignment_tilt_pair import XmippProtAssignmentTiltPair
 from .protocol_align_volume import XmippProtAlignVolume, XmippProtAlignVolumeForWeb
 from .protocol_preprocess.protocol_add_noise import (XmippProtAddNoiseVolumes,
