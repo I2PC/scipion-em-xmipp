59a804a14042d35629e3619f7a1b52751a480f1f
dMaluenda <dmaluendan@gmail.com>
2020-07-01 10:55:27 +0200

    Versioning

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='20.07a8',  # Required
+    version='20.07a9',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
