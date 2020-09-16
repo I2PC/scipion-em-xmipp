c6b1eb28eb30a92a4dd93ec3ffbf2a8ec85e262e
mmmtnez <mmmtnez@cnb.csic.es>
2020-09-16 13:26:40 +0200

    Replaced Chimera by ChimeraX and small correction to color the asymmetric unit

--- xmipp3/viewers/viewer_extract_asymmetric_unit.py
+++ xmipp3/viewers/viewer_extract_asymmetric_unit.py
@@ -58,11 +58,11 @@ class viewerXmippProtExtractUnit(EmProtocolViewer):
         form.addSection(label='Visualization of input volume and extracted '
                               'unit cell')
         form.addParam('displayVol', params.EnumParam,
-                      choices=['chimera', 'slices'], default=VOLUME_CHIMERA,
+                      choices=['chimerax', 'slices'], default=VOLUME_CHIMERA,
                       display=params.EnumParam.DISPLAY_HLIST,
                       label='Display volume with',
-                      help='*chimera*: display volumes as surface with '
-                           'Chimera.\n*slices*: display volumes as 2D slices '
+                      help='*chimerax*: display volumes as surface with '
+                           'ChimeraX.\n*slices*: display volumes as 2D slices '
                            'along z axis.\n')
 
     def _getVisualizeDict(self):
@@ -72,7 +72,7 @@ class viewerXmippProtExtractUnit(EmProtocolViewer):
 
     def _validate(self):
         if find_executable(Chimera.getProgram()) is None:
-            return ["chimera is not available. Either install it or choose"
+            return ["chimerax is not available. Either install it or choose"
                     " option 'slices'. "]
         return []
 
@@ -144,7 +144,7 @@ class viewerXmippProtExtractUnit(EmProtocolViewer):
             f.write("shape icosahedron mesh true radius %(outerRadius)d "
                     "orientation %(symmetry)s\n" % d)
         step = (d['outerRadius'] - d['innerRadius']) / float(len(cMap) - 1)
-        f.write("color radial #3 center 0,0,0 palette -")
+        f.write("color radial #3 center 0,0,0 palette ")
         counter = 0
         s = ""
         for color in cMap:
