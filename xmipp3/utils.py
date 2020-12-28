dcf66344a742ebb033964cf5e090ff03db8c135d
mmmtnez <mmmtnez@cnb.csic.es>
2020-12-28 09:28:27 +0100

    Comment import pvista

--- xmipp3/utils.py
+++ xmipp3/utils.py
@@ -30,7 +30,7 @@ This module contains utils functions for Xmipp protocols
 
 from os.path import join
 import numpy as np
-import pyvista as pv
+# import pyvista as pv
 
 from pyworkflow import Config
 from pwem import emlib
