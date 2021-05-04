7ff940a5e227d80a76722f42d0ea5e2d414c27ef
DaniDelHoyo <danieldelhoyog@gmail.com>
2021-05-04 21:55:11 +0200

    Ignore viewer for protocol_deep_align.py until cv2 dependency is removed.

--- xmipp3/viewers/__init__.py
+++ xmipp3/viewers/__init__.py
@@ -62,7 +62,7 @@ from .viewer_projmatch import XmippProjMatchViewer
 from .viewer_structure_map_sph import XmippProtStructureMapSphViewer
 from .viewer_volume_deform_sph import XmippVolumeDeformSphViewer
 from .viewer_metaprotocol_golden_highres import XmippMetaprotocolGoldenHighResViewer
-from .viewer_deep_align import XmippDeepAlignViewer
+#from .viewer_deep_align import XmippDeepAlignViewer
 
 # TODO: Import from continuousflex the modules needed to create clusters (soft dependency)
 from .viewer_angular_alignment_sph import XmippAngularAlignmentSphViewer
