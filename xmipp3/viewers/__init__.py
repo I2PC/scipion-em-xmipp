e4829e8b811bd2c69f1b679fc21f1e8834c0273b
DavidHerreros <david.herreroscalero@gmail.com>
2021-06-03 14:07:39 +0200

    Remove SPH related code so they are not offered in Scipion for the new Xmipp release

--- xmipp3/viewers/__init__.py
+++ xmipp3/viewers/__init__.py
@@ -59,8 +59,8 @@ from .viewer_volume_consensus import XmippVolumeConsensusViewer
 from .viewer_projmatch import XmippProjMatchViewer
 
 #AJ
-from .viewer_structure_map_sph import XmippProtStructureMapSphViewer
-from .viewer_volume_deform_sph import XmippVolumeDeformSphViewer
+# from .viewer_structure_map_sph import XmippProtStructureMapSphViewer
+# from .viewer_volume_deform_sph import XmippVolumeDeformSphViewer
 from .viewer_metaprotocol_golden_highres import XmippMetaprotocolGoldenHighResViewer
 from .viewer_deep_align import XmippDeepAlignViewer
 
