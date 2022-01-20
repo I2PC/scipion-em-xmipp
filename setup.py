1020d2bedb9d6ac9e722728629a13b6779dbf313
alberto <albertmena@users.noreply.github.com>
2022-01-20 10:23:47 +0100

    broken link

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """A setuptools based setup module.
 
 See:
-https://packaging.python.org/en/latest/distributing.html
+https://packaging.python.org/en/latest/
 https://github.com/pypa/sampleproject
 """
 
