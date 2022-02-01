b766c7c0e156d0ebd76e3eadb0e781dc2d105bde
EstrellaFG <estrellafergi@gmail.com>
2022-02-01 08:57:50 +0100

    protocol_compare_reprojections: improve computation of residuals + tests + fix + formatting (#485)
    * change compute residuals methods + formatting
    
    * mpi 1 for subtraction + let rest of code as in devel
    
    * work in progress
    
    * update test compare reprojections
    
    * default masks for projection subtraction
    
    * fix in average alignment, fix in maskkeep + add new flag for subtract all image
    
    * fix code smell
    
    * fix syntax + extract methods
    
    * add --subAll option to projection subtraction
    
    * fix flag + add help

--- xmipp3/protocols/protocol_compare_reprojections.py
+++ xmipp3/protocols/protocol_compare_reprojections.py
@@ -1,6 +1,8 @@
 # **************************************************************************
 # *
 # * Authors:     Josue Gomez Blanco (josue.gomez-blanco@mcgill.ca)
+# *              Estrella Fernandez Gimenez (me.fernandez@cnb.csic.es)
+# *              (produce residuals improved with projection subtraction)
 # *
 # * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
 # *
@@ -28,8 +30,7 @@ from math import floor
 import os
 
 from pyworkflow import VERSION_1_1
-from pyworkflow.protocol.params import (PointerParam, StringParam, FloatParam,
-                                        BooleanParam)
+from pyworkflow.protocol.params import (PointerParam, StringParam, FloatParam, BooleanParam, IntParam)
 from pyworkflow.utils.path import cleanPath
 from pwem.protocols import ProtAnalysis3D
 from pwem.objects import (SetOfClasses2D, Image, SetOfAverages, SetOfParticles)
@@ -49,6 +50,7 @@ class XmippProtCompareReprojections(ProtAnalysis3D, ProjMatcher):
     The protocol also analyze the covariance matrix of the residual and computes the logarithm of
     its determinant [Cherian2013]. The extremes of this score (called zScoreResCov), that is
     values particularly low or high, may indicate outliers."""
+
     _label = 'compare reprojections'
     _lastUpdateVersion = VERSION_1_1
     
@@ -56,57 +58,69 @@ class XmippProtCompareReprojections(ProtAnalysis3D, ProjMatcher):
         ProtAnalysis3D.__init__(self, **args)
         self._classesInfo = dict()
 
-    #--------------------------- DEFINE param functions --------------------------------------------
+    # --------------------------- DEFINE param functions --------------------------------------------
     def _defineParams(self, form):
         form.addSection(label='Input')
         form.addParam('inputSet', PointerParam, label="Input images", important=True, 
                       pointerClass='SetOfClasses2D, SetOfAverages, SetOfParticles')
         form.addParam('inputVolume', PointerParam, label="Volume to compare images to", important=True,
-                      pointerClass='Volume',
-                      help='Volume to be used for class comparison')
-        form.addParam('useAssignment', BooleanParam, default=True,
-                      label='Use input angular assignment (if available)')
-        form.addParam('optimizeGray', BooleanParam, default=False,
-                      label='Optimize gray scale')
-        form.addParam('ignoreCTF', BooleanParam, default=True,
-                      label='Ignore CTF',
+                      pointerClass='Volume', help='Volume to be used for class comparison')
+        form.addParam('useAssignment', BooleanParam, default=True, label='Use input angular assignment (if available)')
+        form.addParam('optimizeGray', BooleanParam, default=False, label='Optimize gray scale')
+        form.addParam('ignoreCTF', BooleanParam, default=True, label='Ignore CTF',
                       help='By ignoring the CTF you will create projections more similar to what a person expects, '
                            'while by using the CTF you will create projections more similar to what the microscope sees')
         form.addParam('doEvaluateResiduals', BooleanParam, default=False, expertLevel=LEVEL_ADVANCED,
                       label='Evaluate residuals',
-                      help='If this option is chosen, then the residual covariance matrix is calculated and characterized. '
-                           'But this option takes time and disk space')
-        form.addParam('symmetryGroup', StringParam, default="c1",
-                      label='Symmetry group', 
-                      help='See http://xmipp.cnb.uam.es/twiki/bin/view/Xmipp/Symmetry for a description of the symmetry groups format'
-                        'If no symmetry is present, give c1')
+                      help='If this option is chosen, then the residual covariance matrix is calculated and '
+                           'characterized. But this option takes time and disk space')
+        form.addParam('maskVol', PointerParam, pointerClass='VolumeMask', label='Volume mask', allowsNull=True,
+                      condition='doEvaluateResiduals', expertLevel=LEVEL_ADVANCED,
+                      help='3D mask for the input volume. This mask is not mandatory but advisable.')
+        form.addParam('symmetryGroup', StringParam, default="c1", label='Symmetry group',
+                      help='See http://xmipp.cnb.uam.es/twiki/bin/view/Xmipp/Symmetry for a description of the symmetry'
+                           ' groups format. If no symmetry is present, give c1')
         form.addParam('angularSampling', FloatParam, default=5, expertLevel=LEVEL_ADVANCED,
                       label='Angular sampling rate',
                       help='In degrees.'
                       ' This sampling defines how fine the projection gallery from the volume is explored.')
+        form.addParam('resol', FloatParam, label="Filter at resolution: ", default=3, allowsNull=True,
+                      expertLevel=LEVEL_ADVANCED,
+                      help='Resolution (A) at which subtraction will be performed, filtering the volume projections.'
+                           'Value 0 implies no filtering.')
+        form.addParam('sigma', FloatParam, label="Decay of the filter (sigma): ", default=3, condition='resol',
+                      help='Decay of the filter (sigma parameter) to smooth the mask transition',
+                      expertLevel=LEVEL_ADVANCED)
+        form.addParam('iter', IntParam, label="Number of iterations: ", default=5, expertLevel=LEVEL_ADVANCED,
+                      help='Number of iterations for the adjustment process of the images before the subtraction '
+                           'itself. Several iterations are recommended to improve the adjustment.')
+        form.addParam('rfactor', FloatParam, label="Relaxation factor (lambda): ",  expertLevel=LEVEL_ADVANCED,
+                      default=1, help='Relaxation factor for Fourier amplitude projector (POCS), it should be between '
+                                      '0 (no modification of the amplitudes of the volume 2) and 1 (no relaxation).')
         form.addParallelSection(threads=0, mpi=8)
     
-    #--------------------------- INSERT steps functions --------------------------------------------
+    # --------------------------- INSERT steps functions --------------------------------------------
     def _insertAllSteps(self):
-        # Convert input images if necessary
-        self.imgsFn = self._getExtraPath('input_imgs.xmd')
+        """ Convert input images if necessary """
+        self.imgsFn = self._getExtraPath('residuals.xmd')
         vol = self.inputVolume.get()
-        
         self._insertFunctionStep("convertStep", self.imgsFn)
-        imgSet = self.inputSet.get()
-        if not self.useAssignment or isinstance(imgSet, SetOfClasses2D) or isinstance(imgSet, SetOfAverages) or (isinstance(imgSet, SetOfParticles) and not imgSet.hasAlignmentProj()):
+
+        if self._useProjMatching():
             anglesFn = self._getExtraPath('angles.xmd')
-            self._insertFunctionStep("projMatchStep", self.inputVolume.get().getFileName(), self.angularSampling.get(), self.symmetryGroup.get(), self.imgsFn,
-                                     anglesFn, self.inputVolume.get().getDim()[0])
+            self._insertFunctionStep("projMatchStep", self.inputVolume.get().getFileName(), self.angularSampling.get(),
+                                     self.symmetryGroup.get(), self.imgsFn, anglesFn, self.inputVolume.get().getDim()[0])
         else:
-            anglesFn=self.imgsFn
+            anglesFn = self.imgsFn
 
         self._insertFunctionStep("produceResiduals", vol.getFileName(), anglesFn, vol.getSamplingRate())
+
         if self.doEvaluateResiduals.get():
             self._insertFunctionStep("evaluateResiduals")
+
         self._insertFunctionStep("createOutputStep")
 
-    #--------------------------- STEPS functions ---------------------------------------------------
+    # --------------------------- STEPS functions ---------------------------------------------------
     def convertStep(self, imgsFn):
         from ..convert import writeSetOfClasses2D, writeSetOfParticles
         imgSet = self.inputSet.get()
@@ -118,38 +132,41 @@ class XmippProtCompareReprojections(ProtAnalysis3D, ProjMatcher):
         img = ImageHandler()
         fnVol = self._getTmpPath("volume.vol")
         img.convert(self.inputVolume.get(), fnVol)
-        xdim=self.inputVolume.get().getDim()[0]
-        if xdim!=self._getDimensions():
-            self.runJob("xmipp_image_resize","-i %s --dim %d"%(fnVol,self._getDimensions()),numberOfMpi=1)
-    
-    def produceResiduals(self, fnVol, fnAngles, Ts):
-        fnVol = self._getTmpPath("volume.vol")
-        anglesOutFn=self._getExtraPath("anglesCont.stk")
+        xdim = self.inputVolume.get().getDim()[0]
+        if xdim != self._getDimensions():
+            self.runJob("xmipp_image_resize", "-i %s --dim %d" % (fnVol, self._getDimensions()), numberOfMpi=1)
 
-        projectionsOutFn=self._getExtraPath("projections.stk")
-        xdim=self._getDimensions()
-        args="-i %s -o %s --ref %s --optimizeAngles --optimizeShift --max_shift %d --oprojections %s --sampling %f"%\
-                    (fnAngles,anglesOutFn,fnVol,floor(xdim*0.05),projectionsOutFn,Ts)
+    def produceResiduals(self, fnVol, fnAngles, Ts):
+        anglesOutFn = self._getExtraPath("anglesCont.stk")
+        projectionsOutFn = self._getExtraPath("projections.stk")
+        xdim = self._getDimensions()
+        args = "-i %s -o %s --ref %s --optimizeAngles --optimizeShift --max_shift %d --oprojections %s --sampling %f" \
+               % (fnAngles, anglesOutFn, fnVol, floor(xdim*0.05), projectionsOutFn, Ts)
+        self.fnResiduals = self._getExtraPath("residuals.stk")
         if self.doEvaluateResiduals:
-            args+=" --oresiduals %s"%self._getExtraPath("residuals.stk")
+            args += " --oresiduals %s" % self.fnResiduals
+
         if self.ignoreCTF:
-            args+=" --ignoreCTF"
+            args += " --ignoreCTF"
         if self.optimizeGray:
-            args+=" --optimizeGray --max_gray_scale 0.95 "
+            args += "--optimizeGray --max_gray_scale 0.95 "
         self.runJob("xmipp_angular_continuous_assign2", args)
-        fnNewParticles=self._getExtraPath("images.stk")
-        if os.path.exists(fnNewParticles):
-            cleanPath(fnNewParticles)
-    
+
+        if self.doEvaluateResiduals:
+            self._computeResiduals(fnVol)
+
     def evaluateResiduals(self):
         # Evaluate each image
         fnAutoCorrelations = self._getExtraPath("autocorrelations.xmd")
         stkAutoCorrelations = self._getExtraPath("autocorrelations.stk")
-        stkResiduals = self._getExtraPath("residuals.stk")
-        anglesOutFn=self._getExtraPath("anglesCont.xmd")
-        self.runJob("xmipp_image_residuals", " -i %s -o %s --save_metadata_stack %s" % (stkResiduals, stkAutoCorrelations, fnAutoCorrelations), numberOfMpi=1)
-        self.runJob("xmipp_metadata_utilities", '-i %s --operate rename_column "image imageResidual"' % fnAutoCorrelations, numberOfMpi=1)
-        self.runJob("xmipp_metadata_utilities", '-i %s --set join %s imageResidual' % (anglesOutFn, fnAutoCorrelations), numberOfMpi=1)
+        stkResiduals = self.fnResiduals
+        anglesOutFn = self._getExtraPath("anglesCont.xmd")
+        self.runJob("xmipp_image_residuals", " -i %s -o %s --save_metadata_stack %s"
+                    % (stkResiduals, stkAutoCorrelations, fnAutoCorrelations), numberOfMpi=1)
+        self.runJob("xmipp_metadata_utilities", '-i %s --operate rename_column "image imageResidual"'
+                    % fnAutoCorrelations, numberOfMpi=1)
+        self.runJob("xmipp_metadata_utilities", '-i %s --set join %s imageResidual'
+                    % (anglesOutFn, fnAutoCorrelations), numberOfMpi=1)
         cleanPath(fnAutoCorrelations)
     
     def createOutputStep(self):
@@ -180,8 +197,7 @@ class XmippProtCompareReprojections(ProtAnalysis3D, ProjMatcher):
                     outputSet.setAlignmentProj()
             self.iterMd = md.iterRows(imgFn, md.MDL_ITEM_ID)
             self.lastRow = next(self.iterMd)
-            outputSet.copyItems(imgSet,
-                                updateItemCallback=self._processRow)
+            outputSet.copyItems(imgSet, updateItemCallback=self._processRow)
 
         self._defineOutputs(reprojections=outputSet)
         self._defineSourceRelation(self.inputSet, outputSet)
@@ -203,10 +219,10 @@ class XmippProtCompareReprojections(ProtAnalysis3D, ProjMatcher):
             if row.getValue(md.MDL_ITEM_ID) == id:
                 return row
 
-        raise Exception("Missing row %s at %s" % (id,mdFile))
+        raise Exception("Missing row %s at %s" % (id, mdFile))
+
     def _processRow(self, particle, row):
         count = 0
-        
         while self.lastRow and particle.getObjId() == self.lastRow.getValue(md.MDL_ITEM_ID):
             count += 1
             if count:
@@ -215,7 +231,6 @@ class XmippProtCompareReprojections(ProtAnalysis3D, ProjMatcher):
                 self.lastRow = next(self.iterMd)
             except StopIteration:
                 self.lastRow = None
-                    
         particle._appendItem = count > 0
 
     def _createItemMatrix(self, particle, row):
@@ -245,7 +260,66 @@ class XmippProtCompareReprojections(ProtAnalysis3D, ProjMatcher):
             __setXmippImage(emlib.MDL_IMAGE_RESIDUAL)
             __setXmippImage(emlib.MDL_IMAGE_COVARIANCE)
 
-    #--------------------------- INFO functions --------------------------------------------
+    def _useProjMatching(self):
+        """ Determine if it is necessary to perform projection matching step (because there is not input alignment)"""
+        imgSet = self.inputSet.get()
+        if not self.useAssignment or isinstance(imgSet, SetOfClasses2D) \
+                or (isinstance(imgSet, SetOfAverages) and not imgSet.hasAlignmentProj()) or \
+                (isinstance(imgSet, SetOfParticles) and not imgSet.hasAlignmentProj()):
+            return True
+        else:
+            return False
+
+    def _computeResiduals(self, fnVol):
+        vol = self.inputVolume.get().clone()
+        involdim = vol.getDim()
+        if fnVol.endswith('.mrc'):
+            fnVol += ':mrc'
+        program = "xmipp_subtract_projection"
+        args = '-i %s --ref %s -o %s --iter %s --lambda %s --subAll' % \
+               (self.imgsFn, fnVol, self._getExtraPath("residuals"), self.iter.get(), self.rfactor.get())
+        args += ' --saveProj %s' % self._getExtraPath('')
+
+        if self.maskVol.get() is not None:
+            mskVol = self.maskVol.get().getFileName()
+        else:
+            fnDescr = self._getExtraPath("mask.descr")
+            fhDescr = open(fnDescr, 'w')
+            fhDescr.write("%d %d %d 0 \n sph + 1 0 0 0 %d" % (involdim[0], involdim[1], involdim[2], involdim[0] / 2))
+            fhDescr.close()
+            mskVol = self._getExtraPath("mask.mrc")
+            args_mask = "-i %s -o %s" % (fnDescr, mskVol)
+            self.runJob("xmipp_phantom_create", args_mask, numberOfMpi=1)
+            args_imageheader = "-i %s --sampling_rate %f" % (mskVol, vol.getSamplingRate())
+            self.runJob("xmipp_image_header", args_imageheader, numberOfMpi=1)
+        args += ' --maskVol %s' % mskVol
+
+        fnDescr2 = self._getExtraPath("mask.descr2")
+        fhDescr2 = open(fnDescr2, 'w')
+        fhDescr2.write("%d %d %d 0" % (involdim[0], involdim[1], involdim[2]))
+        fhDescr2.close()
+        mskKeep = self._getExtraPath("mask_keep.mrc")
+        args_mask_keep = "-i %s -o %s" % (fnDescr2, mskKeep)
+        self.runJob("xmipp_phantom_create", args_mask_keep, numberOfMpi=1)
+        args_imageheader2 = "-i %s --sampling_rate %f" % (mskKeep, vol.getSamplingRate())
+        self.runJob("xmipp_image_header", args_imageheader2, numberOfMpi=1)
+        args += ' --mask %s' % mskKeep
+
+        resol = self.resol.get()
+        if resol:
+            fc = vol.getSamplingRate() / resol
+            args += ' --cutFreq %f --sigma %d' % (fc, self.sigma.get())
+        self.runJob(program, args, numberOfMpi=1)
+        mrcsresiduals = self._getExtraPath("residuals.mrcs")
+        args2 = " -i %s -o %s" % (mrcsresiduals, self.fnResiduals)
+        self.runJob("xmipp_image_convert", args2, numberOfMpi=1)
+        fnNewParticles = self._getExtraPath("images.stk")
+        if os.path.exists(fnNewParticles):
+            cleanPath(fnNewParticles)
+        if os.path.exists(mrcsresiduals):
+            cleanPath(mrcsresiduals)
+
+    # --------------------------- INFO functions --------------------------------------------
     def _summary(self):
         summary = []
         summary.append("Images evaluated: %i" % self.inputSet.get().getSize())
@@ -255,12 +329,14 @@ class XmippProtCompareReprojections(ProtAnalysis3D, ProjMatcher):
     def _methods(self):
         methods = []
         if hasattr(self, 'outputParticles'):
-            methods.append("We evaluated %i input images %s regarding to volume %s."\
-                           %(self.inputSet.get().getSize(), self.getObjectTag('inputSet'), self.getObjectTag('inputVolume')) )
-            methods.append("The residuals were evaluated according to their mean, variance and covariance structure [Cherian2013].")
+            methods.append("We evaluated %i input images %s regarding to volume %s." %
+                           (self.inputSet.get().getSize(), self.getObjectTag('inputSet'),
+                            self.getObjectTag('inputVolume')))
+            methods.append("The residuals were evaluated according to their mean, variance and covariance structure "
+                           "[Cherian2013].")
         return methods
     
-    #--------------------------- UTILS functions --------------------------------------------
+    # --------------------------- UTILS functions --------------------------------------------
     def _getDimensions(self):
         imgSet = self.inputSet.get()
         if isinstance(imgSet, SetOfClasses2D):
