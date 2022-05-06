346556a419b74cc14354cf24099832c8329f0e1d
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2022-05-06 12:39:02 +0200

    Removed alignment from CL2D

--- xmipp3/protocols/protocol_solid_angles.py
+++ xmipp3/protocols/protocol_solid_angles.py
@@ -281,6 +281,7 @@ class XmippProtSolidAngles(ProtAnalysis3D):
         projRef, self.cl2dIterations, Nclasses)
         args += "--distance correlation --classicalMultiref "
         args += "--maxShift %f " % self.maxShift
+        args += '--dontAlign'
         try:
                 self.runJob("xmipp_classify_CL2D", args)
         except:
