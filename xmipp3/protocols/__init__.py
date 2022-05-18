c1247bd0863fc5bebbe4122092d12696c3fefd6b
Jorge Garcia Condado <jorgegarciacondado@gmail.com>
2022-05-18 15:43:51 +0200

    jorgeDeepHand: Created a protcol to process structures to automatically determine their hand (#489)
    * Consensus clustering using coss method
    
    * Trying to visualize user chosen nclusters
    
    * Viewer for the consensus classes 3D, without apply button
    
    * Update protocol_consensus_classes3D.py
    
    * Removed parent class intialisation for correct working
    
    * Initialize clustering list with initial clusters
    
    * Add dendogram plotting
    
    * Add elbow index by angle of slope
    
    * Add closest distance to origin as elbow
    
    * Add profile log likelihood method
    
    * Add authorship
    
    * Add objective functino plot
    
    * Fix bug of empty sets causing division by zero
    
    * Change definition of elbow angle point
    
    * Fix summary printing
    
    * Changed output that is saved to one set per elbow
    
    * Add error checking of number of clusters
    
    * Add Deep Hand Protocol
    
    * Create form with required inputs
    
    * Make Protocol also an XmippProtocol
    
    * Add hand model
    
    * Add creation of output to store hand
    
    * Add preprocessing
    
    * Change to calling xmippScript
    
    * fixed XmippProtocol call. fixed some code-Smells
    
    * Fix filteredVol problem
    
    * Change xmipp_deep_hand command
    
    * Fix _conda_env bug
    
    * Add flipping of Volume if hand above threshold
    
    * Add validation errors
    
    * Add summary
    
    * Summary fixed
    
    * Add ouptutVol to outputs for all cases
    
    * Remove volumes of no interest at the end
    
    * removing viewer clases3D in the branch; not used
    
    * Help added
    
    Co-authored-by: Daniel del Hoyo <daniel.delhoyo.gomez@alumnos.upm.es>
    Co-authored-by: Carlos Oscar Sorzano <coss@cnb.csic.es>
    Co-authored-by: albertmena <alber.mena@gmail.com>
    Co-authored-by: albertmena <12760268+albertmena@users.noreply.github.com>

--- xmipp3/protocols/__init__.py
+++ xmipp3/protocols/__init__.py
@@ -139,3 +139,4 @@ from .protocol_pdb_deform_zernike3d import XmippProtDeformPDBZernike3D
 from .protocol_volume_adjust_sub import XmippProtVolAdjust, XmippProtVolSubtraction
 from .protocol_volume_consensus import XmippProtVolConsensus
 from .protocol_classes_2d_mapping import XmippProtCL2DMap
+from .protocol_deep_hand import XmippProtDeepHand
