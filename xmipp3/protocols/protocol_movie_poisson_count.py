5ad60e5a7b0654e7e555498ab351888bc9efbb91
danielmarchan3 <danielmarchan3@gmail.com>
2022-07-14 11:40:35 +0200

    Batch processing, optimized and plots

--- xmipp3/protocols/protocol_movie_poisson_count.py
+++ xmipp3/protocols/protocol_movie_poisson_count.py
@@ -24,34 +24,35 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
-
+import matplotlib.pyplot as plt
 import numpy as np
 import os
-import sys
-
 from pyworkflow import VERSION_3_0
 import pyworkflow.utils as pwutils
-from pyworkflow.object import Set, Float
+from pyworkflow.object import Set
 from pyworkflow.protocol import STEPS_PARALLEL
-import pyworkflow.protocol.params as params
-from pyworkflow.protocol.params import (PointerParam, IntParam,
-                                        BooleanParam, LEVEL_ADVANCED)
+from pyworkflow.protocol.params import (PointerParam, IntParam, LEVEL_ADVANCED)
 from pyworkflow.utils.properties import Message
-from pyworkflow.utils.path import moveFile
 import pyworkflow.protocol.constants as cons
 from datetime import datetime
 from pwem.emlib.image import ImageHandler
-from pwem.objects import SetOfMovies, Movie
+from pwem.objects import SetOfMovies
 from pwem.protocols import EMProtocol, ProtProcessMovies
-from xmipp3.convert import setXmippAttribute, getScipionObj
+from xmipp3.convert import getScipionObj
 
+stats_template = {
+    'mean': 0,
+    'std': 0,
+    'var': 0,
+    'max': 0,
+    'min': 0,
+    'per25': 0,
+    'per50': 0,
+    'per75': 0,
+    'per97.5': 0
+             }
 
-### TO DO
-# - Comparar con el dose per frame de cada movie
-# - Arreglar que los steps de mas de 1 movie no de error
-# - El monitor va revisar el umbral en ventanas temporales por si coincide que un grupo de movies ha fallado
-# - Optimizar protocolo
-###
+WINDOW = 2
 
 
 class XmippProtMoviePoissonCount(ProtProcessMovies):
@@ -60,14 +61,18 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
     _lastUpdateVersion = VERSION_3_0
 
     stats = {}
-    estimatedIds2 = []
+    estimatedIds = []
+    meanDoseList = []
+    meanDifferences = []
+    meanDerivatives = []
+    last_multiple = 0
+    meanGlobal = 0
 
 
     def __init__(self, **args):
         EMProtocol.__init__(self, **args)
         self.stepsExecutionMode = STEPS_PARALLEL
-        #self.stepsExecutionMode = params.STEPS_SERIAL
-        self.estimatedIds = []
+
 
     # -------------------------- DEFINE param functions ----------------------
     def _defineParams(self, form):
@@ -96,42 +101,43 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
 
     # -------------------------- STEPS functions ------------------------------
     def createOutputStep(self):
+        # here you would create the plot and then save
+        # print(self.meanDoseList)
+        # plotDoseAnalysis(self.meanDoseList, self.movieStep.get(), self.meanGlobal)
+        # plotDoseAnalysisDiff(self.meanDifferences)
         pass
 
     def _insertAllSteps(self):
+        self.initializeParams()
+        movieSteps = self._insertNewMoviesSteps(self.moviesDict)
+        self._insertFunctionStep('createOutputStep',
+                                 prerequisites=movieSteps, wait=True)
+
+    def initializeParams(self):
+        self.insertedDict = {}
+        self.framesRange = self.inputMovies.get().getFramesRange()
+        self.samplingRate = self.inputMovies.get().getSamplingRate()
+        self.moviesFn = self.inputMovies.get().getFileName()
         # Build the list of all processMovieStep ids by
         # inserting each of the steps for each movie
-        self.insertedDict = {}
-        self.samplingRate = self.inputMovies.get().getSamplingRate()
-
-        self.listOfMovies = [movie.clone() for movie in self.inputMovies.get()]
-
+        self.moviesDict = self._loadInputList()
         self.streamClosed = self.inputMovies.get().isStreamClosed()
         pwutils.makePath(self._getExtraPath('DONE'))
-        # Conversion step is part of processMovieStep because of streaming.
-        movieSteps = self._insertNewMoviesSteps(self.insertedDict,
-                                                self.inputMovies.get())
 
-        finalSteps = self._insertFinalSteps(movieSteps)
-        self._insertFunctionStep('createOutputStep',
-                                 prerequisites=finalSteps, wait=True)
-
-
-    def _insertNewMoviesSteps(self, insertedDict, inputMovies):
+    def _insertNewMoviesSteps(self, inputMovies):
         """ Insert steps to process new movies (from streaming)
         Params:
             insertedDict: contains already processed movies
             inputMovies: input movies set to be check
         """
         deps = []
-        for movie in inputMovies:
-            if movie.getObjId() not in insertedDict:
+        for movie in inputMovies.values():
+            if movie.getObjId() not in self.insertedDict:
                 stepId = self._insertMovieStep(movie)
                 deps.append(stepId)
-                insertedDict[movie.getObjId()] = stepId
+                self.insertedDict[movie.getObjId()] = stepId
 
         return deps
-
 
     def _insertMovieStep(self, movie):
         """ Insert the processMovieStep for a given movie. """
@@ -151,11 +157,10 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
     def _processMovie(self, movie):
         movieId = movie.getObjId()
         if not self.doPoissonCountProcess(movieId):
+            self.info('Movie ID {} not processed'.format(movieId))
             return
 
-        print('entro aqui siendo la movie a procesar:')
-        print(movieId)
-
+        self.info('Movie ID {} processed'.format(movieId))
         self.estimatedIds.append(movieId)
         stats = self.estimatePoissonCount(movie)
         self.stats[movieId] = stats
@@ -180,20 +185,16 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
         steps = self.frameStep.get()
         mean_frames = []
         movieFrames = movieImages.getData()
-        print(np.shape(movieImages.getData()))
         dimx, dimy, z, n = movieImages.getDimensions()
 
         for frame in range(n):
             if (frame % steps) == 0:
-                mean = np.mean(movieFrames[frame, 0, :, :]) #np.mean(coger el slicing del frame)
+                mean = np.mean(movieFrames[frame, 0, :, :])
                 mean_frames.append(mean)
-                print(frame)
-                print(mean)
 
         stats = computeStats(np.asarray(mean_frames))
 
         return stats
-
 
     def _loadOutputSet(self, SetClass, baseName):
         """
@@ -203,7 +204,7 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
         refers to that one.
         """
         setFile = self._getPath(baseName)
-        if os.path.exists(setFile):
+        if os.path.exists(setFile) and os.path.getsize(setFile) > 0:
             outputSet = SetClass(filename=setFile)
             outputSet.loadAllProperties()
             outputSet.enableAppend()
@@ -213,9 +214,22 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
             inputMovies = self.inputMovies.get()
             inputMovies.loadAllProperties()
             outputSet.copyInfo(inputMovies)
+            outputSet.setFramesRange(inputMovies.getFramesRange())
 
         return outputSet
 
+    def _loadInputList(self):
+        """ Load the input set of movies and create a list. """
+        moviesFile = self.inputMovies.get().getFileName()
+        self.debug("Loading input db: %s" % moviesFile)
+        movieSet = SetOfMovies(filename=moviesFile)
+        movieSet.loadAllProperties()
+        newMovies = {movie.getObjId(): movie.clone() for movie in movieSet if movie.getObjId() not in self.insertedDict}
+        self.streamClosed = movieSet.isStreamClosed()
+        movieSet.close()
+        self.debug("Closed db.")
+
+        return newMovies
 
     def _checkNewInput(self):
         # Check if there are new movies to process from the input set
@@ -228,37 +242,23 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
                       pwutils.prettyTime(mTime)))
         # If the input movies.sqlite have not changed since our last check,
         # it does not make sense to check for new input data
-        if self.lastCheck > mTime and hasattr(self, 'listOfMovies'):
+        if self.lastCheck > mTime and hasattr(self, 'moviesDict'):
             return None
 
         self.lastCheck = now
         # Open input movies.sqlite and close it as soon as possible
         newMovies = self._loadInputList()
-
-        self.listOfMovies.extend(newMovies)
-
+        self.moviesDict.update(newMovies)
         outputStep = self._getFirstJoinStep()
 
         if newMovies:
-            fDeps = self._insertNewMoviesSteps(self.insertedDict,
-                                               newMovies)
+            fDeps = self._insertNewMoviesSteps(newMovies)
+
             if outputStep is not None:
                 outputStep.addPrerequisites(*fDeps)
 
             self.updateSteps()
 
-    def _loadInputList(self):
-        """ Load the input set of movies and create a list. """
-        moviesFile = self.inputMovies.get().getFileName()
-        self.debug("Loading input db: %s" % moviesFile)
-        movieSet = SetOfMovies(filename=moviesFile)
-        movieSet.loadAllProperties()
-        newMovies = [movie.clone() for movie in movieSet if movie.getObjId() not in self.insertedDict]
-        self.streamClosed = movieSet.isStreamClosed()
-        movieSet.close()
-        self.debug("Closed db.")
-
-        return newMovies
 
     def _checkNewOutput(self):
         if getattr(self, 'finished', False):
@@ -267,16 +267,15 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
         # Load previously done items (from text file)
         doneList = self._readDoneList()
         # Check for newly done items
-        newDone = [m.clone() for m in self.listOfMovies
-                   if int(m.getObjId()) not in doneList and
-                   self._isMovieDone(m)]
+        newDone = [m.clone() for m in self.moviesDict.values()
+                   if int(m.getObjId()) not in doneList and self._isMovieDone(m)]
 
         allDone = len(doneList) + len(newDone)
         # We have finished when there is not more input movies
         # (stream closed) and the number of processed movies is
         # equal to the number of inputs
         self.finished = self.streamClosed and \
-                        allDone == len(self.listOfMovies)
+                        allDone == len(self.moviesDict)
         streamMode = Set.STREAM_CLOSED if self.finished \
                      else Set.STREAM_OPEN
 
@@ -291,10 +290,13 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
         moviesSet = self._loadOutputSet(SetOfMovies, 'movies.sqlite')
 
         for movie in newDone:
+            movie.setFramesRange(self.framesRange)
             movieId = movie.getObjId()
-            #newMovie = movie.clone()
+            if not self.doPoissonCountProcess(movieId):
+                stats = stats_template
+            else:
+                stats = self.stats[movieId]
 
-            stats = self.stats[movieId]
             mean = stats['mean']
             std = stats['std']
             min = stats['min']
@@ -305,7 +307,40 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
             setAttribute(movie, '_MIN_DOSE_PER_FRAME', min)
             setAttribute(movie, '_MAX_DOSE_PER_FRAME', max)
 
+            if self.doPoissonCountProcess(movieId):
+                self.meanDoseList.append(mean)
+
             moviesSet.append(movie)
+            multiple = len(self.meanDoseList) / WINDOW
+
+            # if len(self.meanDoseList) % WINDOW == 0 and self.last_multiple < multiple:
+            #     meanWindow = np.mean(self.meanDoseList[-WINDOW:])
+            #     print(self.meanDoseList[-WINDOW:])
+            #     meanGlobal = np.mean(self.meanDoseList)
+            #     meanDifference = abs(self.meanDoseList[-WINDOW] - self.meanDoseList[-1]) / meanGlobal #OR meanWindow?
+            #     self.meanDifferences.append(meanDifference)
+            #     print('Multiple: {}  MeanWindow: {}    MeanGlobal: {}    MeanDifference: {}'.format(
+            #         multiple, meanWindow, meanGlobal, meanDifference))
+            #
+            #     self.last_multiple = multiple
+
+            if self.last_multiple < multiple and len(self.meanDoseList) >= 2:
+                meanWindow = np.mean(self.meanDoseList[-2:])
+                print(self.meanDoseList[-2:])
+                self.meanGlobal = np.mean(self.meanDoseList)
+                #meanDifference = abs(self.meanDoseList[-2] - self.meanDoseList[-1]) / meanGlobal  # OR meanWindow?
+                meanDifference = (self.meanDoseList[-1] - self.meanGlobal)/ self.meanGlobal # quitar absoluto
+                self.meanDifferences.append(meanDifference)
+                self.meanDerivatives.append((self.meanDoseList[-1] - self.meanDoseList[-2])/self.meanGlobal)
+
+                print('Multiple: {}  MeanWindow: {}    MeanGlobal: {}    MeanDifference: {}'.format(
+                    multiple, meanWindow, self.meanGlobal, meanDifference))
+
+                plotDoseAnalysis(self.meanDoseList, self.movieStep.get(), self.meanGlobal)
+                plotDoseAnalysisDiff(self.meanDifferences)
+                plotDoseAnalysisDerivatives(self.meanDerivatives)
+
+                self.last_multiple = multiple
 
         self._updateOutputSet('outputMovies', moviesSet, streamMode)
 
@@ -334,7 +369,6 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
         outputSet.close()
 
 
-
     # ------------------------- UTILS functions --------------------------------
     def doPoissonCountProcess(self, movieId):
         return (movieId - 1) % self.movieStep.get() == 0
@@ -343,10 +377,7 @@ class XmippProtMoviePoissonCount(ProtProcessMovies):
     def _validate(self):
         errors = []
         if errors:
-            errors.append("An experimental gain can be associated with a "
-                          "setOfMovies during its importing protocol. "
-                          "Otherwise, no gain reorientation nor "
-                          "gain normalization can be performed.")
+            errors.append("")
         return errors
 
     def _summary(self):
@@ -389,3 +420,29 @@ def computeStats(mean_frames):
              }
     return stats
 
+def plotDoseAnalysis(doseValues, movieStep, meanGlobal):
+    x = np.arange(start=1, stop=len(doseValues)*movieStep, step=movieStep)
+    plt.scatter(x, doseValues)
+    plt.axhline(y=meanGlobal, color='r', linestyle='-')
+    plt.xlabel("Movies ID")
+    plt.ylabel("Dose")
+    plt.title('Dose vs time')
+    plt.show()
+
+def plotDoseAnalysisDiff(meanDifferences):
+    x = np.arange(start=1, stop=len(meanDifferences)+1, step=1)
+    print(x)
+    print(meanDifferences)
+    plt.scatter(x, meanDifferences)
+    plt.xlabel("Differences")
+    plt.ylabel("Dose differences")
+    plt.title('Dose differences with respect to the global mean vs time')
+    plt.show()
+
+def plotDoseAnalysisDerivatives(meanDerivatives):
+    x = np.arange(start=1, stop=len(meanDerivatives)+1, step=1)
+    plt.scatter(x, meanDerivatives)
+    plt.xlabel("Differences")
+    plt.ylabel("Dose mean derivative")
+    plt.title('Dose tendency (last 2 diff) normalized by global mean vs time')
+    plt.show()
