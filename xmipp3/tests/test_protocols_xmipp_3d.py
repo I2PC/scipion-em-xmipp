c196c4a061ca6f98ed6ddafc73ea63fc25bf00ed
gemapatricia <gp.herrera@usp.ceu.es>
2022-07-18 12:53:52 +0200

    Trying to fix GPU values

--- xmipp3/tests/test_protocols_xmipp_3d.py
+++ xmipp3/tests/test_protocols_xmipp_3d.py
@@ -1795,8 +1795,10 @@ class TestXmippScreenDeepLearning(TestXmippBase):
         self.assertIsNotNone(protAddNoise.outputParticles, "There was a problem with add noise protocol")
 
         protScreenDeepLearning = self.newProtocol(XmippProtScreenDeepLearning,
-                                                  USE_GPU=True,
-                                                  GPU_LIST='1',           # MODIFY IF NECESSARY
+                                                  # USE_GPU=True,
+                                                  useGpu=True,
+                                                  # GPU_LIST,
+                                                  gpuList='1',           # MODIFY IF NECESSARY
                                                   inTrueSetOfParticles=protImportParts1.outputParticles,
                                                   numberOfNegativeSets=1,
                                                   negativeSet_1=protAddNoise.outputParticles,
