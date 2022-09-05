162c83bf6ea4d1d520a99f5022da3012e95f8d68
James Krieger <jamesmkrieger@gmail.com>
2022-09-05 22:26:38 +0200

    volume deform viewer support apply protocol

--- xmipp3/viewers/viewer_volume_deform_zernike3d.py
+++ xmipp3/viewers/viewer_volume_deform_zernike3d.py
@@ -32,30 +32,60 @@ import pyworkflow.protocol.params as params
 from pyworkflow.utils import getExt, removeExt
 from os.path import abspath
 from xmipp3.protocols.protocol_volume_deform_zernike3d import XmippProtVolumeDeformZernike3D
+from xmipp3.protocols.protocol_apply_zernike3d import XmippApplyZernike3D
 
 
 class XmippVolumeDeformZernike3DViewer(ProtocolViewer):
     """ Visualize the output of protocol volume strain """
     _label = 'viewer volume deform sph'
-    _targets = [XmippProtVolumeDeformZernike3D]
+    _targets = [XmippProtVolumeDeformZernike3D, XmippApplyZernike3D]
     _environments = [DESKTOP_TKINTER, WEB_DJANGO]
     OPEN_FILE = "open %s\n"
     VOXEL_SIZE = "volume #%d voxelSize %s\n"
     VOL_HIDE = "vol #%d hide\n"
     VIEW = "view\n"
 
+    if isinstance(self.protocol, XmippApplyZernike3D):
+        self.only_apply = True
+        self.deformed = self.protocol.deformed.get()
+        self.have_set = isinstance(self.deformed, Set)
+    else:
+        self.have_set = False
+
     def _defineParams(self, form):
         form.addSection(label='Show deformation')
+        form.addParam('idChoice', params.EnumParam,
+                      condition='self.have_set==True',
+                      choices=list(self.deformed.getIdSet()), default=0,
+                      label='Structure to display', display=params.EnumParam.DISPLAY_COMBO,
+                      help='Select which volume to display from the IDs of the set')
         form.addParam('doShowStrain', params.LabelParam,
+                      condition='self.only_apply==False',
                       label="Display the strain deformation")
         form.addParam('doShowRotation', params.LabelParam,
+                      condition='self.only_apply==False',
                       label="Display the rotation deformation")
         form.addParam('doShowMorphOrigRef', params.LabelParam,
+                      condition='self.only_apply==False',
                       label="Display the morphing between original and reference volumes")
         form.addParam('doShowMorphDeformedRef', params.LabelParam,
                       label="Display the morphing between deformed and reference volumes")
 
     def _getVisualizeDict(self):
+        if isinstance(self.protocol, XmippApplyZernike3D):
+            if self.have_set==True:
+                self.inputVol = self.protocol.volume[list(self.deformed.getIdSet())[self.pdbIdChoice]]
+                self.chosen = self.deformed[list(self.deformed.getIdSet())[self.pdbIdChoice]]
+            else:
+                self.inputVol = self.protocol.volume
+                self.chosen = self.deformed
+
+            myDict = {
+                'fnRefVol': self.inputVol.getFileName(),
+                'fnOutVol': self.chosen.getFileName()
+                    }
+            self.protocol._updateFilenamesDict(myDict)
+
         self.protocol._createFilenameTemplates()
         return {'doShowStrain': self._doShowStrain,
                 'doShowRotation': self._doShowRotation,
