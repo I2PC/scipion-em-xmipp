1551ff4e3a8e7cc10bbb92a2f3716c022b5f4866
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2022-12-19 10:12:32 +0100

    Added merging step

--- xmipp3/protocols/protocol_consensus_classes.py
+++ xmipp3/protocols/protocol_consensus_classes.py
@@ -28,6 +28,7 @@
 
 from typing import Set
 import collections
+import itertools
 
 from pwem.protocols import EMProtocol
 from pwem.objects import SetOfClasses
@@ -36,6 +37,9 @@ from pyworkflow.protocol.params import Form, MultiPointerParam
 from pyworkflow.constants import BETA
 
 from xmipp3.convert import setXmippAttribute
+
+import numpy as np
+import sklearn.metrics
 
 
 class XmippProtConsensusClasses(EMProtocol):
@@ -61,7 +65,7 @@ class XmippProtConsensusClasses(EMProtocol):
     # --------------------------- INSERT steps functions -----------------------
     def _insertAllSteps(self):
         self._insertFunctionStep('intersectStep')
-        self._insertFunctionStep('ensembleClustersStep')
+        self._insertFunctionStep('mergeStep')
 
     def intersectStep(self):
         classifications = self._getInputClassifications()
@@ -82,8 +86,16 @@ class XmippProtConsensusClasses(EMProtocol):
         # Stablish source output relationships
         self._defineSourceRelation(self.inputClassifications, outputClasses)
 
-    def ensembleClustersStep(self):
+    def mergeStep(self):
+        classifications = self._getInputClassifications()
         intersections = self._convertClassification(self.outputClasses_intersections)
+        
+        # Merge all intersections
+        merged = self._mergeIntersections(
+            classifications,
+            intersections, 
+            self._mergeObjectiveFunctionEntropy
+        )
 
     def createOutputStep(self):
         pass
@@ -146,6 +158,90 @@ class XmippProtConsensusClasses(EMProtocol):
             result = intersections
 
         return result
+    
+    def _calculateClusterSimilarity(self, x, y):
+        xIds = x.getIds()
+        yIds = y.getIds()
+        nCommon = len(xIds & yIds)
+        nTotal = min(len(xIds), len(yIds))
+        return nCommon / nTotal
+    
+    def _calculateClusterSimilarityMatrix(self, a, b):
+        result = np.empty((len(a), len(b)))
+        
+        for i, x in enumerate(a):
+            for j, y in enumerate(b):
+                result[i,j] = self._calculateClusterSimilarity(x, y)
+
+        return result
+    
+    def _calculateClusterCosineSimilarityMatrix(self, allClassifications, intersections):
+        similarityMatrix = self._calculateClusterSimilarityMatrix(allClassifications, intersections)
+        similarityMatrix /= np.linalg.norm(similarityMatrix, axis=0)
+        return np.dot(similarityMatrix.T, similarityMatrix)
+        
+    def _calculateClusterEntropy(self, intersections):
+        p = np.array(list(map(len, intersections)), dtype=float)
+        p /= np.sum(p)
+        entropy = -np.sum(p*np.log2(p))
+        assert(entropy >= 0) # Entropy cannot be negative
+        return entropy
+        
+    def _mergeIntersectionPair(self, intersections, indices):
+        """ Return a list of subsets where the intersections[indices] have been merged """
+        # Divide the input intersections into the ones referred by indices and not
+        selection = [intersections[i] for i in indices]  # intersection[indices]
+        result = [intersections[i] for i in range(len(intersections)) if i not in indices]  # intersections - selection
+
+        # Merge all the selected clusters into the same one
+        merged = XmippProtConsensusClasses.Cluster.union(*selection)
+        
+        # Add the merged items to the result
+        result.append(merged)
+        return result
+    
+    def _mergeObjectiveFunctionEntropy(self, intersections, pair, similarity):
+        h = self._calculateClusterEntropy(intersections)
+        hi = self._calculateClusterEntropy(self._mergeIntersectionPair(intersections, pair))
+        return (h - hi) / similarity
+        
+    def _mergeCheapestIntersectionPair(self, allClassifications, intersections, objectiveFunc):
+        """ Merges the most similar pair of intersections """
+        # Calculate distance matrix
+        similarities = self._calculateClusterCosineSimilarityMatrix(allClassifications, intersections)
+
+        # For each possible pair of intersections compute the cost of merging them
+        obValues = {}
+        for pair in itertools.combinations(range(len(intersections)), r=2):
+            obValues[pair] = objectiveFunc(intersections, pair, similarities[pair])
+
+        # Select the minimum cost pair and merge them
+        mergePair, mergeObValue = min(obValues.items(), key=lambda pair : pair[1])
+        mergedIntersections = self._mergeIntersectionPair(intersections, mergePair)
+
+        return mergedIntersections, mergeObValue
+
+    def _mergeIntersections(self, classifications, intersections, objectiveFunc):
+        """Iteratively merge intersection pairs where the 
+        objective function is the least """
+        # Results
+        allIntersections = [intersections]
+        allObValues = [0.0]
+
+        # Obtain a list with all source classes
+        allClassifications = list(itertools.chain(*classifications))
+        
+        # Calculate similarity matrix
+
+        # Iteratively merge intersections
+        while len(allIntersections[-1]) > 1:
+            mergedIntersections, obValue = self._mergeCheapestIntersectionPair(allClassifications, allIntersections[-1], objectiveFunc)
+            allIntersections.append(mergedIntersections)
+            allObValues.append(obValue)
+
+        assert(len(allIntersections) == len(allObValues))
+        return allIntersections, allObValues
+    
     
     def _createOutputClasses(self, 
                              classification: SetOfClasses,
