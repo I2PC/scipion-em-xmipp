cb3556f9a0a85b3e6c15bc682f92e8bda13bdc27
EstrellaFG <estrellafergi@gmail.com>
2023-05-03 09:39:38 +0200

    fixing the fix

--- xmipp3/protocols/protocol_preprocess/protocol_create_mask3d.py
+++ xmipp3/protocols/protocol_preprocess/protocol_create_mask3d.py
@@ -392,13 +392,11 @@ sph + 1 '3.03623188  0.02318841 -5.04130435' '7'
             messages.append("And, we smoothed it (sigma=%f voxels)."
                             % self.sigmaConvolution.get())
         if self.hasAttribute('outputMask'):
-            messages.append('We refer to the output mask as %s.'
-            
+            messages.append('We refer to the output mask as %s.')
+
     def _validate(self):
         errors = []
         if self.source == SOURCE_VOLUME:
             if not self.inputVolume.get():
-        errors.append("You need to select an input volume")
-                            % self.outputMask.getNameId())
-        return messages
-    
+                errors.append("You need to select an input volume" % self.outputMask.getNameId())
+                return errors
