5fea75006a6ec4bec9e47586b825e2f57498be01
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-09-06 08:58:03 +0000

    Initial commit

---
+++ xmipp3/protocols/protocol_aligned_solid_angles.py
@@ -0,0 +1,214 @@
+# **************************************************************************
+# *
+# * Authors:     C.O.S. Sorzano (coss@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+import os.path
+
+from pwem.protocols import ProtAnalysis3D
+from pwem.objects import (Volume, FSC, SetOfVolumes, Class3D, 
+                          SetOfParticles, SetOfClasses3D, Particle,
+                          Pointer, SetOfFSCs )
+
+from pyworkflow import BETA
+from pyworkflow.protocol.params import (Form, PointerParam, 
+                                        FloatParam, IntParam,
+                                        StringParam, BooleanParam,
+                                        MultiPointerParam, EnumParam,
+                                        GT, GE, Range,
+                                        LEVEL_ADVANCED, USE_GPU, GPU_LIST )
+
+from xmipp3.convert import readSetOfParticles, writeSetOfParticles, rowToParticle
+
+class XmippProtAlignedSolidAngles(ProtAnalysis3D):
+    _label = 'aligned solid angles'
+    _conda_env = 'xmipp_swiftalign'
+    _devStatus = BETA
+
+    def __init__(self, *args, **kwargs):
+        ProtAnalysis3D.__init__(self, *args, **kwargs)
+
+    # --------------------------- DEFINE param functions ------------------------
+    def _defineParams(self, form: Form):
+        form.addHidden(USE_GPU, BooleanParam, default=True,
+                       label="Use GPU for execution",
+                       help="This protocol has both CPU and GPU implementation.\
+                       Select the one you want to use.")
+
+        form.addHidden(GPU_LIST, StringParam, default='0',
+                       expertLevel=LEVEL_ADVANCED,
+                       label="Choose GPU IDs",
+                       help="Add a list of GPU devices that can be used")
+
+        form.addSection(label='Input')
+        form.addParam('inputParticles', PointerParam, label='Particles', important=True,
+                      pointerClass=SetOfParticles,
+                      help='Input particle set')
+        form.addParam('inputVolume', PointerParam, label='Volume', important=True,
+                      pointerClass=Volume,
+                      help='Input volume')
+        form.addParam('symmetryGroup', StringParam, label='Symmetry group', default='c1')
+
+
+        form.addSection(label='CTF')
+        form.addParam('considerInputCtf', BooleanParam, label='Consider CTF',
+                      default=True,
+                      help='Consider the CTF of the particles')
+
+        form.addSection(label='Angular neighborhood')
+        form.addParam('angularSampling', FloatParam, label='Angular sampling',
+                      default=5.0, validators=[Range(0,180)],
+                      help='Angular sampling interval in degrees')
+        form.addParam('angularDistance', FloatParam, label='Angular distance',
+                      default=10.0, validators=[Range(0,180)],
+                      help='Maximum angular distance in degrees')
+        
+        form.addParallelSection(threads=0, mpi=8)
+
+        form.addSection(label='Compute')
+        form.addParam('batchSize', IntParam, label='Batch size', 
+                      default=1024,
+                      help='It is recommended to use powers of 2. Using numbers around 8192 works well')
+        form.addParam('copyParticles', BooleanParam, label='Copy particles to scratch', default=False, 
+                      help='Copy input particles to scratch directory. Note that if input file format is '
+                      'incompatible the particles will be converted into scratch anyway')
+        
+    # --------------------------- INSERT steps functions ------------------------
+    def _insertAllSteps(self):
+        self._insertFunctionStep('convertInputStep')
+
+        if self.considerInputCtf:
+            self._insertFunctionStep('correctCtfStep')
+
+        self._insertFunctionStep('projectGalleryStep')
+        self._insertFunctionStep('angularNeighborhoodStep')
+
+    # --------------------------- STEPS functions -------------------------------
+    def convertInputStep(self):
+        particles: SetOfParticles = self.inputParticles.get()
+        
+        writeSetOfParticles(particles, 
+                            self._getInputParticleMdFilename())
+
+        def is_mrc(path: str) -> bool:
+            _, ext = os.path.splitext(path)
+            return ext == '.mrc' or ext == '.mrcs'
+        
+        # Convert to MRC if necessary
+        if self.copyParticles or not all(map(is_mrc, particles.getFiles())):
+            args = []
+            args += ['-i', self._getInputParticleMdFilename()]
+            args += ['-o', self._getInputParticleStackFilename()]
+            args += ['--save_metadata_stack', self._getInputParticleMdFilename()]
+            args += ['--keep_input_columns']
+            args += ['--track_origin']
+
+            self.runJob('xmipp_image_convert', args, numberOfMpi=1)
+    
+    def correctCtfStep(self):
+        particles: SetOfParticles = self.inputParticles.get()
+        acquisition = particles.getAcquisition()
+        
+        # Perform a CTF correction using Wiener Filtering
+        args = []
+        args += ['-i', self._getInputParticleMdFilename()]
+        args += ['-o', self._getWienerParticleMdFilename()]
+        args += ['--pixel_size', self._getSamplingRate()]
+        args += ['--spherical_aberration', acquisition.getSphericalAberration()]
+        args += ['--voltage', acquisition.getVoltage()]
+        if particles.isPhaseFlipped():
+            args +=  ['--phase_flipped']
+
+        args += ['--batch', self.batchSize]
+        if self.useGpu:
+            args += ['--device'] + self._getDeviceList()
+
+        env = self.getCondaEnv()
+        env['LD_LIBRARY_PATH'] = '' # Torch does not like it
+        self.runJob('xmipp_swiftalign_wiener_2d', args, numberOfMpi=1, env=env)
+
+    def projectGalleryStep(self):
+        args = []
+        args += ['-i', self._getInputVolumeFilename()]
+        args += ['-o', self._getGalleryStackFilename()]
+        args += ['--sampling_rate', self._getAngularSampling()]
+        args += ['--angular_distance', self._getAngularDistance()]
+        args += ['--sym', self._getSymmetryGroup()]
+        args += ['--method', 'fourier', 1, 0.25, 'bspline'] 
+        args += ['--compute_neighbors']
+        args += ['--max_tilt_angle', 90]
+        args += ['--experimental_images', self._getInputParticleMdFilename()]
+
+        # Create a gallery of projections of the input volume
+        # with the given angular sampling
+        self.runJob("xmipp_angular_project_library", args)
+        
+    def angularNeighborhoodStep(self):
+        args = []
+        args += ['--i1', self._getInputParticleMdFilename()]
+        args += ['--i2', self._getGalleryMdFilename()]
+        args += ['-o', self._getNeighborsMdFilename()]
+        args += ['--dist', self._getAngularDistance()]
+        args += ['--sym', self._getSymmetryGroup()]
+
+        # Compute several groups of the experimental images into
+        # different angular neighbourhoods
+        self.runJob("xmipp_angular_neighbourhood", args, numberOfMpi=1)
+
+    # --------------------------- UTILS functions -------------------------------
+    def _getSamplingRate(self):
+        return float(self.inputParticles.get().getSamplingRate())
+    
+    def _getSymmetryGroup(self):
+        return self.symmetryGroup.get()
+    
+    def _getAngularSampling(self):
+        return self.angularSampling.get()
+    
+    def _getAngularDistance(self):
+        return self.angularDistance.get()
+    
+    def _getInputVolumeFilename(self):
+        return self.inputVolume.get().getFilename()
+    
+    def _getInputParticleMdFilename(self):
+        return self._getPath('input_particles.xmd')
+    
+    def _getInputParticleStackFilename(self):
+        return self._getTmpPath('input_particles.mrcs')
+
+    def _getWienerParticleMdFilename(self):
+        return self._getExtraPath('particles_wiener.xmd')
+
+    def _getWienerParticleStackFilename(self):
+        return self._getExtraPath('particles_wiener.mrcs')
+
+    def _getGalleryMdFilename(self):
+        return self._getExtraPath('gallery.xmd')
+
+    def _getGalleryStackFilename(self):
+        return self._getExtraPath('gallery.mrcs')
+
+    def _getNeighborsMdFilename(self):
+        return self._getExtraPath('neighbors.xmd')
