aab6cdee917c04c9cc32679908cdcd888492876e
MartinSalinas98 <ssalinasmartin@gmail.com>
2023-11-17 16:28:07 +0100

    Read from CIF

--- xmipp3/protocols/protocol_validate_fscq.py
+++ xmipp3/protocols/protocol_validate_fscq.py
@@ -165,7 +165,7 @@ class XmippProtValFit(ProtAnalysis3D):
     def convertInputStep(self):
         """ Convert inputs to desired format."""
         #Convert Input to pdb
-        if self.getInputStructFile().endswith('.pdb') or self.getInputStructFile().endswith('.ent'):
+        if self.getInputStructFile().endswith('.pdb') or self.getInputStructFile().endswith('.ent') or self.getInputStructFile().endswith('.cif'):
             os.symlink(os.path.abspath(self.getInputStructFile()), self.getPDBFile())
         else:
             toPdb(self.getInputStructFile(), self.getPDBFile())
