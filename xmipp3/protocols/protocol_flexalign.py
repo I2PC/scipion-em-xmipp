086db1a2083cc6e0b1adf4a8dc0cc1bdf20d56c2
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-12-11 12:19:29 +0000

    Error message

--- xmipp3/protocols/protocol_flexalign.py
+++ xmipp3/protocols/protocol_flexalign.py
@@ -70,7 +70,7 @@ class XmippProtFlexAlign(ProtAlignMovies):
         EER_CONDITION = 'inputMovies is not None and len(inputMovies) > 0 and inputMovies.getFiles().pop().endswith(".eer")'
 
         form.addParam('nFrames', params.IntParam, label='Number of EER frames',
-                      condition=EER_CONDITION, default=40, validators=[params.GT(0)],
+                      condition=EER_CONDITION, default=40, validators=[params.GT(0, "Number of EER frames must be a positive integer (> 0).")],
                       help='Number of frames to be generated. EER files contain subframes, that will be grouped into the selected number of frames.')
 
         # FlexAlign does not support cropping
