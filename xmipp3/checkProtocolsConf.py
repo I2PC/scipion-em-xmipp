68b4565b90fe2978e91c36e9d00c4e05239c851b
albertmena <12760268+albertmena@users.noreply.github.com>
2024-12-03 13:53:05 +0100

    adding list test pending

--- xmipp3/checkProtocolsConf.py
+++ xmipp3/checkProtocolsConf.py
@@ -29,6 +29,7 @@ Usage: python check_missing_protocols.py
 
 import ast
 import glob
+import os
 
 def get_classes_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -42,6 +43,8 @@ with open("protocols.conf", 'r', encoding='utf-8') as file:
 
 blackList = ['ScatterImageMarker','XMIPPCOLUMNS']
 
+
+#######Missing classes
 missingCounter = 0
 classCounter = 0
 for py_file in glob.glob("protocols/*.py"):
@@ -52,3 +55,59 @@ for py_file in glob.glob("protocols/*.py"):
                 print(f"Missing: {class_name} from {py_file}")
                 missingCounter +=1
 print(f"Missing classes: {missingCounter}/{classCounter}")
+
+
+
+#######Missing tests
+
+def extract_protocols_from_file(file_path):
+	"""Extrae las llamadas a self.newProtocol en un archivo."""
+	protocols = []
+	with open(file_path, 'r', encoding='utf-8') as f:
+		source_code = f.read()
+	tree = ast.parse(source_code)
+	for node in ast.walk(tree):
+		if isinstance(node, ast.Call):
+			if (isinstance(node.func, ast.Attribute) and
+					node.func.attr == "newProtocol" and
+					isinstance(node.func.value, ast.Name) and
+					node.func.value.id == "self"):
+				if node.args:
+					protocol_node = node.args[0]
+					if isinstance(protocol_node, ast.Name):
+						protocol = protocol_node.id
+					elif isinstance(protocol_node, ast.Attribute):
+						protocol = protocol_node.attr
+					elif isinstance(protocol_node, ast.Constant):
+						protocol = protocol_node.value
+					if protocol not in protocols:
+						protocols.append(protocol)
+	return protocols
+
+
+def extract_protocols_from_folder(folder_path):
+	"""Extrae todos los protocolos de los archivos .py en una carpeta."""
+	protocolsFull= []
+	for root, _, files in os.walk(folder_path):
+		for file in files:
+			if file.endswith(".py"):
+				file_path = os.path.join(root, file)
+				protocols = extract_protocols_from_file(file_path)
+				for protocol in protocols:
+					if protocol not in protocolsFull:
+						protocolsFull.append(protocol)
+	return protocolsFull
+
+folder_path = "tests/"
+protocols = extract_protocols_from_folder(folder_path)
+partProt = protocols.index('xpsp')
+protocols[partProt] = 'XmippProtScreenParticles'
+
+protocolsWithoutTest = []
+for py_file in glob.glob("protocols/*.py"):
+    for class_name in get_classes_from_file(py_file):
+	    if class_name not in protocols:
+		    protocolsWithoutTest.append(class_name)
+		    
+print(f"\nProtocolos without test ({len(protocolsWithoutTest)}/{len(protocols)})")
+print('\n'.join(protocolsWithoutTest))
