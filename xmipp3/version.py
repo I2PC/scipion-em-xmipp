177442ea55c5017030e9dcbc52411acf57fe24d6
albertmena <12760268+albertmena@users.noreply.github.com>
2024-12-17 12:45:25 +0100

    tag name fix

--- xmipp3/version.py
+++ xmipp3/version.py
@@ -27,7 +27,8 @@
 
 type_of_version = 'release' #'release' 'devel'
 _logo = "xmipp_logo" + ("" if type_of_version == 'release' else '_devel') + '.png'
-_current_xmipp_tag = 'devel'
 _currentBinVersion = '3.24.12.0'
+_current_xmipp_tag = 'v' + _currentBinVersion #'devel'
+
 _currentDepVersion = '1.0'
 __version__ = _currentBinVersion[2:] + ".0"  # Set this to ".0" on each xmipp binary release, otherwise increase it --> ".1", ".2", ...
