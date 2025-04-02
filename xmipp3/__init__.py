2e6ebcc3ea9a3c8d3728f418757a8ffad7273c6d
alberto <alber.mena@gmail.com>
2025-04-02 12:57:19 +0200

    Update __init__.py

--- xmipp3/__init__.py
+++ xmipp3/__init__.py
@@ -172,7 +172,7 @@ class Plugin(pwem.Plugin):
                 tar='void.tgz',
                 commands=commands.getCommands(),
                 neededProgs=['conda'],
-                default=False
+                default=True
             )
         
         if develMode:
