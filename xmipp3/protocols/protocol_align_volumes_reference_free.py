0e82c0563ca935743a4ecf868c296fcd5e673753
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaiia.eu>
2025-06-18 19:44:14 +0000

    Using external synchronization script

--- xmipp3/protocols/protocol_align_volumes_reference_free.py
+++ xmipp3/protocols/protocol_align_volumes_reference_free.py
@@ -27,11 +27,12 @@
 
 from typing import List
 import itertools
+import math
 import numpy as np
 import scipy.sparse
 
 from pyworkflow.protocol.params import (MultiPointerParam, FloatParam, EnumParam,
-                                        GE, Range)
+                                        IntParam, GE, GT, Range)
 
 from pyworkflow import BETA, UPDATED, NEW, PROD
 from pwem.objects import Volume, SetOfVolumes
@@ -73,6 +74,8 @@ class XmippProtAlignVolumesReferenceFree(ProtAnalysis3D):
                       validators=[Range(0, 90.0)], default=60.0 )
         form.addParam('missingRegion', EnumParam, label='Missing region',
                       choices=self.CHOICES)
+        form.addParam('connectivity', IntParam, label='Conectivity',
+                      validators=[GT(0)])
         form.addParallelSection(mpi=8)
 
     # --------------------------- INSERT steps functions -----------------------
@@ -88,7 +91,7 @@ class XmippProtAlignVolumesReferenceFree(ProtAnalysis3D):
     def makePairsStep(self):
         volumes = self._getInputVolumes()
         n = len(volumes)
-        
+        k = self.connectivity.get()
         
         program = 'xmipp_transform_filter'
         args = []
@@ -104,21 +107,37 @@ class XmippProtAlignVolumesReferenceFree(ProtAnalysis3D):
         args += ['--save', self._getMultiplicityMaskFilename()]
         self.runJob(program, args, numberOfMpi=1)
         
+        jumps = {1}
+        if k > 1:
+            n_2 = math.floor(n/2)
+            r = (n_2 / (k-1))
+            for i in range(1, k):
+                jumps.add(max(round(r*i), 1))
+            
+        pairs = set()
+        for i in range(n):
+            for jump in jumps:
+                first = i
+                second = (i+jump) % n
+                pairs.add((min(first, second), max(first, second)))
+        
         md = emlib.metadata.MetaData()
-        for index0, index1 in itertools.combinations(range(n), r=2):
+        for index0, index1 in pairs:
             objId = md.addObject()
-            volume0 = volumes[index0]
-            volume1 = volumes[index1]
-            md.setValue(emlib.metadata.MDL_IMAGE_REF, emlib.image.ImageHandler.locationToXmipp(volume0.getLocation()), objId)
-            md.setValue(emlib.metadata.MDL_IMAGE, emlib.image.ImageHandler.locationToXmipp(volume1.getLocation()), objId)
+            volume0: Volume = volumes[index0]
+            location0 = emlib.image.ImageHandler.locationToXmipp(volume0.getLocation())
+            md.setValue(emlib.metadata.MDL_REF, index0, objId)
+            md.setValue(emlib.metadata.MDL_IMAGE_REF, location0, objId)
+
+            volume1: Volume = volumes[index1]
+            location1 = emlib.image.ImageHandler.locationToXmipp(volume1.getLocation())
+            md.setValue(emlib.metadata.MDL_IDX, index1, objId)
+            md.setValue(emlib.metadata.MDL_IMAGE, location1, objId)
+
             md.setValue(emlib.metadata.MDL_MASK, self._getMultiplicityMaskFilename(), objId)
-            md.setValue(emlib.metadata.MDL_COMMENT, '%06d,%06d' % (index0, index1), objId)
         md.write(self._getExtraPath('pairs.xmd'))
         
     def pairwiseAlignStep(self):
-        volumes = self._getInputVolumes()
-        n = len(volumes)
-        
         program = 'xmipp_tomo_align_subtomo_pairs'
         args = []
         args += ['-i', self._getPairMetadataFilename()]
@@ -128,80 +147,44 @@ class XmippProtAlignVolumesReferenceFree(ProtAnalysis3D):
         args += ['--keep_input_columns']
         self.runJob(program, args)
         
-        rotations = np.empty((3*n, 3*n))
-        shifts = np.empty((3, n, n))
-        md = emlib.metadata.MetaData(self._getAlignedPairMetadataFilename())
+    def synchronizeStep(self):
+        volumes = self._getInputVolumes()
+
+        program = 'xmipp_synchronize_transform'
+        args = []
+        args += ['-i', self._getAlignedPairMetadataFilename()]
+        args += ['-o', self._getSynchronizedMetadataFilename()]
+        self.runJob(program, args, numberOfMpi=1)
+        
+        md = emlib.metadata.MetaData(self._getSynchronizedMetadataFilename())
         for objId in md:
-            comment: str = md.getValue(emlib.metadata.MDL_COMMENT, objId)
+            itemId = md.getValue(emlib.metadata.MDL_ITEM_ID, objId)
+            volume: Volume = volumes[itemId]
+            location = emlib.image.ImageHandler.locationToXmipp(volume.getLocation())
+            md.setValue(emlib.metadata.MDL_IMAGE, location, objId)
+        md.write(self._getSynchronizedMetadataFilename())
+        
+    def applyTransformationsStep(self):
+        md = emlib.metadata.MetaData(self._getSynchronizedMetadataFilename())
+        
+        program = 'xmipp_transform_geometry'
+        for i, objId in enumerate(md):
+            inputFn = md.getValue(emlib.metadata.MDL_IMAGE, objId)
             rot = md.getValue(emlib.metadata.MDL_ANGLE_ROT, objId)
             tilt = md.getValue(emlib.metadata.MDL_ANGLE_TILT, objId)
             psi = md.getValue(emlib.metadata.MDL_ANGLE_PSI, objId)
             x = md.getValue(emlib.metadata.MDL_SHIFT_X, objId)
             y = md.getValue(emlib.metadata.MDL_SHIFT_Y, objId)
             z = md.getValue(emlib.metadata.MDL_SHIFT_Z, objId)
-            
-            index0, index1 = comment.split(',')
-            index0 = int(index0)
-            index1 = int(index1)
-            
-            start0 = 3*index0
-            end0 = start0 + 3
-            start1 = 3*index1
-            end1 = start1 + 3
-
-            rotation = xmippLib.Euler_angles2matrix(rot, tilt, psi)
-            shift = np.array([x, y, z])
-            
-            rotations[start0:end0, start1:end1] = rotation
-            rotations[start1:end1, start0:end0] = rotation.T
-            
-            shifts[:,index0,index1] = -(rotation.T @ shift)
-            shifts[:,index1,index0] = shift
-            
-        for index in range(n):
-            start = 3*index
-            end = start + 3
-            rotations[start:end,start:end] = np.eye(3)
-            shifts[:,index,index] = 0
-            
-        np.save(self._getPairwiseRotationMatrixFilename(), rotations)
-        np.save(self._getPairwiseShiftMatrixFilename(), shifts)
-        
-    def synchronizeStep(self):
-        n = self._getVolumeCount()
-        pairwiseRotations = np.load(self._getPairwiseRotationMatrixFilename())
-        pairwiseShifts = np.load(self._getPairwiseShiftMatrixFilename())
-        
-        rotations, w = self._decomposeRotations(pairwiseRotations, n)
-        shifts, err = self._decomposeShifts(pairwiseShifts, rotations)
-
-        print('Rotation consistency: %f' % (w.sum() / (3*n)))
-        print('Shift consistency: %f' % err)
-        
-        transforms = np.empty((n, 4, 4))
-        transforms[:,:3,:3] = rotations
-        transforms[:,:3,3] = shifts
-        transforms[:,3,:3] = 0
-        transforms[:,3,3] = 1
-        
-        np.save(self._getTransformationMatrixFilename(), transforms)
-        
-    def applyTransformationsStep(self):
-        volumes = self._getInputVolumes()
-        transforms = np.load(self._getTransformationMatrixFilename())
-        transforms = np.linalg.inv(transforms)
-        
-        program = 'xmipp_transform_geometry'
-        for i, volume, transform in zip(itertools.count(), volumes, transforms):
-            inputVolumeFilename = emlib.image.ImageHandler.locationToXmipp(volume.getLocation())
-            outputVolumeFilename = self._getTransoformedVolumeFilename(i)
+            outputFn = self._getTransoformedVolumeFilename(i)
             
             args = []
-            args += ['-i', inputVolumeFilename]
-            args += ['-o', outputVolumeFilename]
-            args += ['--matrix']
-            args += [','.join(map(str, transform.flatten()))]
-            self.runJob(program, args)
+            args += ['-i', inputFn]
+            args += ['-o', outputFn]
+            args += ['--rotate_volume', 'euler', rot, tilt, psi]
+            args += ['--shift', x, y, z]
+            args += ['--inverse']
+            self.runJob(program, args, numberOfMpi=1)
             
     def averageVolumesStep(self):
         n = self._getVolumeCount()
@@ -295,63 +278,15 @@ class XmippProtAlignVolumesReferenceFree(ProtAnalysis3D):
     def _getAlignedPairMetadataFilename(self):
         return self._getExtraPath('aligned_pairs.xmd')
 
+    def _getSynchronizedMetadataFilename(self):
+        return self._getExtraPath('synchronized.xmd')
+
     def _getMultiplicityMaskFilename(self):
         return self._getExtraPath('multiplicity.mrc')
-    
-    def _getPairwiseRotationMatrixFilename(self) -> str:
-        return self._getExtraPath('pairwise_rotations.npy')
-    
-    def _getPairwiseShiftMatrixFilename(self) -> str:
-        return self._getExtraPath('pairwise_shifts.npy')
-    
-    def _getTransformationMatrixFilename(self) -> str:
-        return self._getExtraPath('transforms.npy')
-    
-    def _getRotatedVolumeFilename(self, i: int) -> str:
-        return self._getExtraPath('rotated_volume_%06d.mrc' % i)
     
     def _getTransoformedVolumeFilename(self, i: int) -> str:
         return self._getExtraPath('transformed_volume_%06d.mrc' % i)
     
     def _getAverageFilename(self):
         return self._getExtraPath('average.mrc')
-    
-    def _decomposeRotations(self, pairwise: np.ndarray, n: int):
-        w, v = np.linalg.eigh(pairwise)
-        w = w[-3:]
-        v = v[:,-3:]
-        v *= np.sqrt(w)
-        
-        matrices = v.reshape(n, 3, 3)
-        u, _, vh = np.linalg.svd(matrices, full_matrices=True)
-        matrices = u @ vh
-
-        return matrices, w
-    
-    def _decomposeShifts(self, pairwise: np.ndarray, rotations: np.ndarray):
-        n = len(rotations)
-        nCols = 3*n
-        nRows = 3*n*(n-1) // 2
-
-        desing = scipy.sparse.lil_array((nRows, nCols))
-        y = np.empty(nRows)
-        for i, (index0, index1) in enumerate(itertools.combinations(range(n), r=2)):
-            startRow = 3*i
-            endRow = startRow + 3
-            start0 = 3*index0
-            end0 = start0 + 3
-            start1 = 3*index1
-            end1 = start1 + 3
-            
-            desing[startRow:endRow,start0:end0] = -rotations[index1] @ rotations[index0].T
-            desing[startRow:endRow,start1:end1] = np.eye(3)
-            y[startRow:endRow] = pairwise[:,index0,index1]
-        desing = desing.tocoo()
-        
-        result = scipy.sparse.linalg.lsqr(desing, y)
-        x = result[0]
-        err = result[3]
-        shifts = x.reshape((n, 3))
-        
-        return shifts, err
     
