716b4dbd1ee854ad24d8201a3d2d418d1fa8375b
albertmena <12760268+albertmena@users.noreply.github.com>
2025-06-25 16:49:59 +0200

    +=

--- xmipp3/protocols/protocol_deep_hand.py
+++ xmipp3/protocols/protocol_deep_hand.py
@@ -177,7 +177,7 @@ class XmippProtDeepHand(EMProtocol, XmippProtocol):
     def _validate(self):
         errors = []
 
-        errors.append(self.validateDLtoolkit())
+        errors += self.validateDLtoolkit()
 
         if self.thresholdAlpha.get() > 1.0 or self.thresholdAlpha.get() < 0.0:
            errors.append("Alpha threshold must be between 0.0 and 1.0")
