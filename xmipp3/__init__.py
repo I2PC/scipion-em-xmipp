e13c2e6f564565e90163aca8c3ebfc297395be67
alberto <alber.mena@gmail.com>
2026-02-03 11:25:15 +0000

    xmipp3_installer bia pypi and not github (#995)

--- xmipp3/__init__.py
+++ xmipp3/__init__.py
@@ -178,15 +178,12 @@ class Plugin(pwem.Plugin):
                 neededProgs=['conda'],
                 default=True
             )
-        xmipp3_installer_cmd = f'pip install "xmipp3-installer @ git+{XMIPP3_INSTALLER_URL}@{_xmipp3_installerV}" '
         if develMode:
             xmippSrc = 'xmippDev'
             installCommands = [
-		        (f'{xmipp3_installer_cmd } && '
-				 f'cd {pwem.Config.EM_ROOT} && rm -rf {xmippSrc} && '
+		        (f'cd {pwem.Config.EM_ROOT} && rm -rf {xmippSrc} && '
 		         f'git clone {XMIPP_GIT_URL} {xmippSrc} && '
 		         f'cd {xmippSrc} && '
-		         #f'git checkout {branchTest} && '
 		         f'./xmipp ', COMPILE_TARGETS)
 	        ]
 
@@ -201,8 +198,7 @@ class Plugin(pwem.Plugin):
         else:
             xmippSrc = f'xmipp3-{version._binVersion}'
             installCommands = [
-                (f'{xmipp3_installer_cmd } && '
-				 f'cd .. && rm -rf {xmippSrc} && '
+                (f'cd .. && rm -rf {xmippSrc} && '
                  f'git clone {XMIPP_GIT_URL} -b {version._binVersion} {xmippSrc} && '
                  f'cd {xmippSrc} && '
                  f'./xmipp', COMPILE_TARGETS)
