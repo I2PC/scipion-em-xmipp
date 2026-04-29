cb7e0beeb956aef4a3d1ad0175415e7446167645
agarcia <alber.mena@gmail.com>
2026-04-29 11:29:36 +0000

    definning coords for streaming

--- xmipp3/protocols/protocol_deep_micrograph_screen.py
+++ xmipp3/protocols/protocol_deep_micrograph_screen.py
@@ -254,7 +254,7 @@ class XmippProtDeepMicrographScreen(ProtExtractParticles, XmippProtocol):
           if self.useGpu.get():
               gpuId = self.setGPU(oneGPU=False)
               args += f' -g {gpuId} '
-
+          self.info(f'args: {args}')
           self.runJob('xmipp_deep_micrograph_cleaner', args)
 
 
@@ -576,7 +576,7 @@ class XmippProtDeepMicrographScreen(ProtExtractParticles, XmippProtocol):
         """
         micFn = mic.getFileName()
         micBase = pwutils.removeBaseExt(os.path.basename(micFn))
-
+        coords = None
         posFn = self._getExtraPath('outputCoords', micBase + '.pos')
         maskFn = self._getExtraPath('predictedMasks', micBase + '.mrc')
         thumbFn = self._getExtraPath('thumbnails', micBase + '.png')
@@ -639,7 +639,7 @@ class XmippProtDeepMicrographScreen(ProtExtractParticles, XmippProtocol):
         if coords:
             patches = [Circle((x, y), radius=radius) for x, y in coords]
             collection = PatchCollection(patches, edgecolor='red', facecolor='none', linewidth=1)
-        ax.add_collection(collection)
+            ax.add_collection(collection)
 
         # --- Save thumbnail ---
         plt.savefig(thumbFn, dpi=80, bbox_inches=None, pad_inches=0, pil_kwargs={"compress_level": 4})
